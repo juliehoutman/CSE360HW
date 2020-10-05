e26cba1284d23306b7bd7d91522e2d55522ba839
Julie Houtman <houtman.julie@gmail.com>
2020-10-04 19:11:00 -0700

    The first version of the AddingMachine.java

---
+++ CSE360-1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,27 @@
+package cse360assignment02;
+//first version
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  //gets the total
+  public int getTotal () {
+    return 0;
+  }
+  //adds the values
+  public void add (int value) {
+  }
+  //subtracts the values
+  public void subtract (int value) {
+  }
+  //makes the total a string
+  public String toString () {
+    return "";
+  }
+  //clears the total
+  public void clear() {
+  }
+}
