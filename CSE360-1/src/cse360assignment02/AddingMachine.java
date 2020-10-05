e397f0028f3438b3366308d015da8d7e21ae27c5
Julie Houtman <houtman.julie@gmail.com>
2020-10-05 10:23:10 -0700

    first change to the addingmachine.java

--- CSE360-1/src/cse360assignment02/AddingMachine.java
+++ CSE360-1/src/cse360assignment02/AddingMachine.java
@@ -3,25 +3,33 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   //gets the total
   public int getTotal () {
-    return 0;
+    return total;
   }
   //adds the values
   public void add (int value) {
+	  total = total + value;
+	  history = history + " " + "+" + " " + value;
+	  
+	  
   }
   //subtracts the values
   public void subtract (int value) {
+	  total = total - value;
+	  history = history + " " + "-" + " " + value;
   }
   //makes the total a string
   public String toString () {
-    return "";
+    return history;
   }
   //clears the total
   public void clear() {
+	  total = 0;
   }
 }
